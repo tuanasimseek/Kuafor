73522595d89dd64e40b49c71c192bec677e4a58d
semanurcaan <semanurnurcan@gmail.com>
2025-12-13 23:25:58 +0300

    s

--- kuafor_app/windows/flutter/generated_plugins.cmake
+++ kuafor_app/windows/flutter/generated_plugins.cmake
@@ -3,10 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_secure_storage_windows
+  firebase_core
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
